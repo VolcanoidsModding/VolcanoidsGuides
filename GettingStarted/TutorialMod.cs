1a24830b8e68a2a0ec1ca7ae435304bb77337f12
MelodicAlbuild <43552157+MelodicAlbuild@users.noreply.github.com>
2021-04-10 01:28:18 -0500

    Add GettingStarted

---
+++ GettingStarted/TutorialMod.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TutorialMod.Guides.GettingStarted
+{
+    public class TutorialMod : GameMod
+    {
+        public override void Load()
+        {
+            var version = typeof(TutorialMod).Assembly.GetName().Version;
+            var lastWrite = File.GetLastWriteTime(typeof(TutorialMod).Assembly.Location);
+            Debug.Log($"TutorialMod Loaded: {version}, Build Time: {lastWrite.ToShortTimeString()}");
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode arg1)
+        {
+            switch (scene.name)
+            {
+                case "MainMenu":
+                    GameObject.Find("EarlyAccess").gameObject.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = "TutorialMod";
+                    break;
+                case "Island":
+                    break;
+            }
+        }
+
+        public override void Unload()
+        {
+        }
+    }
+}
