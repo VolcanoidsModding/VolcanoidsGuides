a1f0342266048435fe97e488511bd497c2fd010b
MelodicAlbuild <43552157+MelodicAlbuild@users.noreply.github.com>
2021-05-04 15:36:31 -0500

    Create TutorialMod.cs

---
+++ CreateItem/TutorialMod.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VolcanoidsSDK;
+
+namespace TutorialMod.Guides.InitSDK
+{
+    class TutorialMod : GameMod
+    {
+        public SDK volcSDK;
+        public override void Load()
+        {
+            var version = typeof(TutorialMod).Assembly.GetName().Version;
+            var lastWrite = File.GetLastWriteTime(typeof(TutorialMod).Assembly.Location);
+            Debug.Log($"TutorialMod Loaded: {version}, Build Time: {lastWrite.ToShortTimeString()}");
+
+            volcSDK = new SDK("TutorialMod", GUID.Parse("abcdefg123456"));
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode arg1)
+        {
+            switch (scene.name)
+            {
+                case "MainMenu":
+                    GameObject.Find("EarlyAccess").gameObject.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = "TutorialMod";
+                    break;
+                case "Island":
+                    Functions volcFunctions = new Functions(volcSDK);
+                    break;
+            }
+        }
+
+        public override void Unload()
+        {
+        }
+    }
+}
