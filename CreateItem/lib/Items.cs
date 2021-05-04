18973a2eac3aa3ba0128bf09f3730c1972f4a46b
MelodicAlbuild <43552157+MelodicAlbuild@users.noreply.github.com>
2021-05-04 15:38:16 -0500

    Create Items.cs

---
+++ CreateItem/lib/Items.cs
@@ -0,0 +1,12 @@
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace TutorialMod.lib
+{
+    class QuestingItems
+    {
+    }
+}
