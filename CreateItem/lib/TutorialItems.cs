16f8b773c807dff77a40455099f7047718a6f15e
MelodicAlbuild <43552157+MelodicAlbuild@users.noreply.github.com>
2021-06-23 19:17:30 -0400

    Create an Item

--- CreateItem/lib/TutorialItems.cs
+++ CreateItem/lib/TutorialItems.cs
@@ -10,7 +10,7 @@ namespace TutorialMod.lib
     class TutorialItems
     {
         public static void InitItems(Functions volcFuncts) {
-            volcFuncts.CreateItem("");
+            volcFuncts.CreateItem("CobaltIngot", 99, "Cobalt Ingot", "A Cobalt Ingot", "069306E234D4499C9F5A3526D0E5C4D3", "TitaniumIngot", "Resources/Ingots/CobaltIngot.png");
         }
     }
 }
